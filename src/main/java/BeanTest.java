c73461b4e95a411210ad2ab7c3228da0c009e9a8
Damian Wrather <damian.wrather@gmail.com>
2023-05-11 10:31:29 -0500

    JavaBeans

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -7,9 +7,10 @@ import java.util.ArrayList;
 public class BeanTest {
     public static void main(String[] args) {
         Albums album = new Albums(1, "Michael Jackson", "Thriller", "30-Nov-82", 65, "Pop");
-        Author author = new Author("R.L. Stine");
-        Quote quote1 = new Quote("I'm a vegetarian", author);
-        Quote quote2 = new Quote("I'm a vegetarian", author);
+        Author author1 = new Author("R.L. Stine");
+        Author author2 = new Author("J.K. Rowling");
+        Quote quote1 = new Quote("I'm a vegetarian", author1);
+        Quote quote2 = new Quote("I'm a wizard", author2);
 
         ArrayList<Quote> quotes = new ArrayList<Quote>();
         quotes.add(quote1);

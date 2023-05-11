f4867437a10b897425543d4fb53bb9ebf7b010a8
Damian Wrather <damian.wrather@gmail.com>
2023-05-11 09:41:19 -0500

    JavaBeans

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,23 @@
+import models.Albums;
+import models.Author;
+import models.Quote;
+
+import java.util.ArrayList;
+
+public class BeanTest {
+    public static void main(String[] args) {
+        Albums album = new Albums(1, "Michael Jackson", "Thriller", "30-Nov-82", 65, "Pop");
+        Author author = new Author("R.L. Stine");
+        Quote quote1 = new Quote("I'm a vegetarian", "R.L. Stine");
+        Quote quote2 = new Quote("I'm a vegetarian", "R.L. Stine");
+
+        ArrayList<Quote> quotes = new ArrayList<Quote>();
+        quotes.add(quote1);
+        quotes.add(quote2);
+
+        for(Quote quote : quotes){
+            System.out.printf("Quote: %s\nAuthor: %s\n\n", quote.getQuote(), quote.getAuthor());
+        }
+    }
+
+}
---
+++ src/main/java/models/Quote.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Quote {
+    private String quote;
+    private String author;
+
+    public Quote() {
+    }
+
+    public Quote(String quote, String author) {
+        this.quote = quote;
+        this.author = author;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
